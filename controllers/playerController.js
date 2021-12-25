7ba5a0b3435fcd71eecc821bbc1b6dd5239a8e5a
Virgiawan Listiyandi <imvirgiawan17@gmail.com>
2021-12-25 19:04:56 +0700

    Tambah payment method di response detail page

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -23,14 +23,23 @@ const landing_page = async (req, res) => {
 const detail_page = async (req, res) => {
   try {
     const { id } = req.params;
+
     const voucher = await Voucher.findOne({ _id: id })
       .populate("category")
       .populate("user", "_id name phoneNumber")
       .populate("nominals");
+    const payment = await Payment.find();
+
     if (!voucher) {
       return res.status(404).json({ message: "Voucher not found" });
     }
-    res.status(200).json({ data: voucher });
+
+    res.status(200).json({
+      data: {
+        detail: voucher,
+        payments: payment,
+      },
+    });
   } catch (err) {
     res.status(500).json({ message: err.message || "Server error" });
   }
